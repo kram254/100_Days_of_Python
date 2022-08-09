64ee3604debb61afd1701eb75ae17d1e6a1efb11
kram254 <markorlando45@gmail.com>
2022-08-09 14:55:43 +0300

    Just another day - Day 5

---
+++ linked_lists.py
@@ -0,0 +1,23 @@
+"""
+Linked Lists are made up of nodes. Each of these nodes stores data and a pointer to the next node in the linked list
+In order to access the third item in the list, we first have to start from the head, then traverse our way through the list to the 3rd item
+There is no way to start in the middle like in a list because our first pointer points to the first node in the linked list
+
+Big O Notation
+This is used to describe the performance of an algorithm in the worst-case scenario (the scenario where it will run the slowest)
+Allows us to compare different operations on different data structures like linked-lists and arrays
+
+
+Data Strustures that make use of linked lists
+1. Stack
+Allows items to be placed at the top of other items ... The user can only access the item on top.
+For someone to access an item that is below the top (head), they first have to access the one on top then thereafter now access the others
+Stacks uses the LIFO mechanism which is Last item In will be the First item Out.
+
+2. Queue
+A queue works just the same as the stack except only that in a queue an item can only be added after the last item a process called 
+enqueueing and one can only remove items from the front... a process called dequeueing...
+ Queues use FIFO which is First In First Out algorithm...
+ 
+
+"""
